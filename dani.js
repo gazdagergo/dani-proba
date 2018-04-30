db8a597ee6972fcae0c61c1a2be9df3e64d9560e
gazdagergo <info@gazdagergo.com>
2018-04-30 10:18:55 +0200

    file of dani

---
+++ dani.js
@@ -0,0 +1,8 @@
+
+document.write("itt vagyok here im");
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+document.getElementById("demo1").innerHTML = fruits;
+document.getElementById("demo2").innerHTML = fruits.pop();
+document.getElementById("demo3").innerHTML = fruits;
+
